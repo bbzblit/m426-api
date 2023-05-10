093b8b8474c0d27b9e45bb79a0a0584a9696e789
Yannick Müller <yannick.mueller@fusiva.ch>
2023-05-10 15:56:59 +0200

    add initial setup

---
+++ src/main/java/dev/bbzblit/m426/M426Application.java
@@ -0,0 +1,13 @@
+package dev.bbzblit.m426;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M426Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M426Application.class, args);
+	}
+
+}
