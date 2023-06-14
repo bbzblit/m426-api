94768c287ab291f93c9f50869875cc20a87cfc0d
Yannick Müller <mail@ynnk.dev>
2023-06-14 14:39:03 +0200

    add method to check login status

--- src/main/java/dev/bbzblit/m426/service/SessionService.java
+++ src/main/java/dev/bbzblit/m426/service/SessionService.java
@@ -17,18 +17,17 @@ public class SessionService {
     private final SessionRepository sessionRepository;
     private final UserService userService;
 
-    public SessionService(final SessionRepository sessionRepository,final UserService userService){
+    public SessionService(final SessionRepository sessionRepository, final UserService userService) {
         this.sessionRepository = sessionRepository;
         this.userService = userService;
     }
 
 
-
-    public Session login(LoginModel loginModel){
-        try{
+    public Session login(LoginModel loginModel) {
+        try {
             User user = userService.findUserByUsername(loginModel.getUsername());
             byte[] salt = user.getSaltValue();
-            if(HashService.verifyPassword(loginModel.getPassword(), salt, user.getPassword())){
+            if (HashService.verifyPassword(loginModel.getPassword(), salt, user.getPassword())) {
 
                 RandomString randomString = new RandomString();
                 randomString.nextString();
@@ -36,11 +35,11 @@ public class SessionService {
                 Session session = new Session(randomString.nextString(), user, LocalDateTime.now().plusHours(2));
                 return this.sessionRepository.save(session);
             }
-        } catch (ResponseStatusException ex){ /*NotFound exception should be wrong password or username */}
+        } catch (ResponseStatusException ex) { /*NotFound exception should be wrong password or username */}
         throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Wrong Password or Username");
     }
 
-    public Session getSessionByToken(String token){
+    public Session getSessionByToken(String token) {
         return this.sessionRepository.findSessionByTokenAndExpirationDateIsAfter(token,
                 LocalDateTime.now()).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.UNAUTHORIZED,
@@ -49,9 +48,30 @@ public class SessionService {
     }
 
 
-    public void logout(String token){
+    public void logout(String token) {
         getSessionByToken(token);
         this.sessionRepository.deleteById(token);
     }
+
+    public void isLoggedIn(final String token) {
+        if (getSessionByToken(token).getUser() == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,
+                    "You need to be logged in to use this app");
+        }
+    }
+
+    public void isAdministrator(final String token) {
+        User user = getSessionByToken(token).getUser();
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,
+                    "You need to be logged in to use this app");
+        }
+
+        if (!user.isEmployee()) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+                    "You need to be employee to access this Endpoint");
+        }
+    }
+
 
 }
