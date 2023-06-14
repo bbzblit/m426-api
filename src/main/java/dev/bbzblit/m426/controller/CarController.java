44fdcb39095d7870ce3f08d9eccae5aa03e201e0
lev1nn <levinsch@bluewin.ch>
2023-06-14 15:02:27 +0200

    Add security check in controllers

--- src/main/java/dev/bbzblit/m426/controller/CarController.java
+++ src/main/java/dev/bbzblit/m426/controller/CarController.java
@@ -2,6 +2,7 @@ package dev.bbzblit.m426.controller;
 
 import dev.bbzblit.m426.entity.Car;
 import dev.bbzblit.m426.service.CarService;
+import dev.bbzblit.m426.service.SessionService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,39 +14,45 @@ import java.util.List;
 @RestController
 @Validated
 public class CarController {
+    public  final SessionService sessionService;
     public final CarService service;
 
-    public CarController(final CarService carService) {
+    public CarController(SessionService sessionService, final CarService carService){
+        this.sessionService = sessionService;
         this.service = carService;
     }
 
     @GetMapping("/api/v1/car")
-    public ResponseEntity<Iterable<Car>> all() {
+    public ResponseEntity<Iterable<Car>> all(@CookieValue("session") String session) {
+        sessionService.isAdministrator(session);
         List<Car> cars = service.getCars();
         return new ResponseEntity<>(cars, HttpStatus.OK);
     }
 
     @GetMapping("/api/v1/car/{id}")
-    public ResponseEntity<Car> one(@PathVariable Long id) {
+    public ResponseEntity<Car> one(@PathVariable Long id, @CookieValue("session") String session) {
+        sessionService.isAdministrator(session);
         Car car = service.getCar(id);
         return new ResponseEntity<>(car, HttpStatus.OK);
     }
 
     @PostMapping("/api/v1/car")
-    public ResponseEntity<Car> insert(@RequestBody Car car) {
+    public ResponseEntity<Car> insert(@RequestBody Car car, @CookieValue("session") String session) {
+        sessionService.isAdministrator(session);
         Car newCar = service.insertCar(car);
         return new ResponseEntity<>(newCar, HttpStatus.CREATED);
     }
 
     @PutMapping("/api/v1/car/{id}")
-    public ResponseEntity<Car> update(@RequestBody @Valid Car car, @PathVariable Long id) {
+    public ResponseEntity<Car> update(@RequestBody @Valid Car car, @PathVariable Long id, @CookieValue("session") String session) {
+        sessionService.isAdministrator(session);
         Car updatedCar = service.updateCar(car, id);
         return new ResponseEntity<>(updatedCar, HttpStatus.OK);
     }
 
     @DeleteMapping("/api/v1/car/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
+    public void delete(@PathVariable Long id, @CookieValue("session") String session) {
+        sessionService.isAdministrator(session);
         service.deleteAddress(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/dev/bbzblit/m426/controller/ReservationController.java
+++ src/main/java/dev/bbzblit/m426/controller/ReservationController.java
@@ -2,6 +2,7 @@ package dev.bbzblit.m426.controller;
 
 import dev.bbzblit.m426.entity.Reservation;
 import dev.bbzblit.m426.service.ReservationService;
+import dev.bbzblit.m426.service.SessionService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,30 +10,33 @@ import java.util.List;
 
 @RestController
 public class ReservationController {
-
+    public  final SessionService sessionService;
     private final ReservationService reservationService;
 
 
-    public ReservationController(ReservationService reservationService){
+    public ReservationController(SessionService sessionService, ReservationService reservationService){
+        this.sessionService = sessionService;
         this.reservationService = reservationService;
     }
 
 
     @PostMapping("/api/v1/reservation")
-    public Reservation insertReservation(@RequestBody @Valid Reservation reservation,
-                                         @CookieValue("session") String token){
-        return this.reservationService.saveReservation(reservation, token);
+    public Reservation insertReservation(@RequestBody @Valid Reservation reservation, @CookieValue("session") String session){
+        sessionService.isLoggedIn(session);
+        return this.reservationService.saveReservation(reservation, session);
     }
 
     @GetMapping("/api/v1/reservation")
-    public List<Reservation> getReservationsInFuture(@CookieValue("session") String token){
-        return this.reservationService.getNextReservations(token);
+    public List<Reservation> getReservationsInFuture(@CookieValue("session") String session){
+        sessionService.isLoggedIn(session);
+        return this.reservationService.getNextReservations(session);
     }
 
 
     @DeleteMapping("/api/v1/reservation/{id}")
-    public void deleteReservationById(@CookieValue("session") String token, @PathVariable("id") Long id){
-        this.reservationService.revokeReservation(token, id);
+    public void deleteReservationById(@CookieValue("session") String session, @PathVariable("id") Long id){
+        sessionService.isLoggedIn(session);
+        this.reservationService.revokeReservation(session, id);
     }
 
 
